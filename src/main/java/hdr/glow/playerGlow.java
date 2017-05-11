b1345ab4bd3b6f5ef64086e8f934585acdd944ca
MrHDR <refshet@yahoo.no>
2017-05-11 07:29:34 +0200

    Added Groundwork for Color Changes.

--- src/main/java/hdr/glow/playerGlow.java
+++ src/main/java/hdr/glow/playerGlow.java
@@ -4,13 +4,22 @@ import hdr.glow.commands.glowToggle;
 import org.spongepowered.api.effect.potion.PotionEffect;
 import org.spongepowered.api.effect.potion.PotionEffectTypes;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameLoadCompleteEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.scoreboard.Scoreboard;
+import org.spongepowered.api.scoreboard.Team;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.format.TextColors;
 
-@Plugin(id = "playerglow", name = "Player Glow", version = "0.5")
+import java.util.ArrayList;
+import java.util.List;
+
+import static sun.misc.Version.print;
+
+@Plugin(id = "playerglow", name = "Player Glow", version = "0.7")
 public class playerGlow {
 
     private void makeCommands() {
@@ -22,12 +31,33 @@ public class playerGlow {
         Sponge.getCommandManager().register(this, toggleCMD, "toggleglow", "tgw");
     }
 
+    private void makeTeams() {
+        Team blackTeam = Team.builder()
+                .name("blackGlow")
+                .color(TextColors.BLACK)
+                .build();
+        Team dBlueTeam = Team.builder()
+                .name("darkblueGlow")
+                .color(TextColors.DARK_BLUE)
+                .build();
+        List<Team> teamList = new ArrayList<>();
+        teamList.add(blackTeam);
+        teamList.add(dBlueTeam);
+        Scoreboard.builder().teams(teamList).build();
+    }
+
+
     public static PotionEffect glowPot;
 
     @Listener
     public void onInit(GameStartedServerEvent e){
         makeCommands();
         glowPot = PotionEffect.builder().potionType(PotionEffectTypes.GLOWING).duration(100000).amplifier(100).particles(false).ambience(true).build();
+    }
+
+    @Listener
+    public void onInit(GameLoadCompleteEvent e){
+        makeTeams();
     }
 
     }
