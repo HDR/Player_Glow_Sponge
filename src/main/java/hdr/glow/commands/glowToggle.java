70e8a105271cb7dbccae539e6172a280b57ca1d7
Adam Spofford <pieflavor.mc@gmail.com>
2017-05-27 23:57:30 -0700

    Fix toggling glow off removing all potion effects

--- src/main/java/hdr/glow/commands/glowToggle.java
+++ src/main/java/hdr/glow/commands/glowToggle.java
@@ -5,37 +5,27 @@ import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
-import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.mutable.PotionEffectData;
+import org.spongepowered.api.effect.potion.PotionEffectTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.effect.potion.PotionEffect;
-import org.spongepowered.api.effect.potion.PotionEffectTypes;
-import org.spongepowered.api.data.manipulator.mutable.PotionEffectData;
 import org.spongepowered.api.text.chat.ChatTypes;
 import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.format.TextStyles;
 
 import static hdr.glow.playerGlow.glowPot;
-import java.util.List;
 
 public class glowToggle implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         if (src instanceof Player) {
             Player player = (Player) src;
-            if (player.get(Keys.POTION_EFFECTS).isPresent()) {
-                List<PotionEffect> potionEffects = player.get(Keys.POTION_EFFECTS).get();
-                for (PotionEffect pot : potionEffects) {
-                    if (pot.getType().equals(PotionEffectTypes.GLOWING)) {
-                        List<PotionEffect> effects = player.get(Keys.POTION_EFFECTS).get();
-                        effects.clear();
-                        player.offer(Keys.POTION_EFFECTS, effects);
-                        player.sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.RED, TextStyles.BOLD, "Glow Disabled"));
-                    }
-
-                }
+            PotionEffectData effects = player.getOrCreate(PotionEffectData.class).get();
+            if (effects.asList().stream().anyMatch(pot -> pot.getType().equals(PotionEffectTypes.GLOWING))) {
+                effects.removeAll(pot -> pot.getType().equals(PotionEffectTypes.GLOWING));
+                player.offer(effects);
+                player.sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.RED, TextStyles.BOLD, "Glow Disabled"));
             } else {
-                PotionEffectData effects = player.getOrCreate(PotionEffectData.class).get();
                 effects.addElement(glowPot);
                 player.offer(effects);
                 player.sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.GREEN, TextStyles.BOLD, "Glow Enabled"));
